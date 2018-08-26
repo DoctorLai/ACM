d38d903fc1617871913de0d610e2ca7dc911a52a
@justyy <dr.zhihua.lai@gmail.com>
2018-08-26 12:13:04 +0100

    Create 1000.cpp

---
+++ timus/1000. A-B Problem/1000.cpp
@@ -0,0 +1,12 @@
+// https://helloacm.com/coding-exercise-timus-online-judge-1000-ab-problem-cc-solutions-with-assembly/
+// http://acm.timus.ru/problem.aspx?space=1&num=1000
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+  return 0;
+}
