3acb8bf3ac11a7904d7fc96a8f8e4347bfa2f0be
@justyy <dr.zhihua.lai@gmail.com>
2018-08-26 12:02:04 +0100

    Create 867.cpp

---
+++ leetcode/867. Transpose Matrix/867.cpp
@@ -0,0 +1,18 @@
+// https://helloacm.com/how-to-transpose-a-2d-matrix/
+https://leetcode.com/problems/transpose-matrix/description/
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        int rows = A.size();
+        if (rows == 0) return {{}};
+        int cols = A[0].size();
+        vector<vector<int>> r(cols, vector<int>(rows));        
+        for (int i = 0; i < rows; ++ i) {
+            for (int j = 0; j < cols; ++ j) {
+                r[j][i] = A[i][j];
+            }
+        }        
+        return r;
+    }
+};
