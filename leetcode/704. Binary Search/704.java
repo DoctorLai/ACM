6a52dfdc3f82fa694e9027df97edc2d3004bd01f
justyy <dr.zhihua.lai@gmail.com>
2018-12-08 09:21:14 +0000

    Binary search in java

---
+++ leetcode/704. Binary Search/704.java
@@ -0,0 +1,19 @@
+// https://helloacm.com/the-binary-search-in-java/
+// https://leetcode.com/problems/binary-search/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] > target) {
+                j = mid - 1;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
