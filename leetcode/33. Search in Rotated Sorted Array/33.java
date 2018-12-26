f800c1d958959474246f1c80d0f1cc327be1d175
justyy <dr.zhihua.lai@gmail.com>
2018-12-26 20:41:06 +0000

    33

---
+++ leetcode/33. Search in Rotated Sorted Array/33.java
@@ -0,0 +1,29 @@
+// https://helloacm.com/the-binary-search-algorithm-for-rotated-sorted-array/
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int H = nums.length - 1;
+        int low = 0, high = H;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (nums[mid] < nums[high]) {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
