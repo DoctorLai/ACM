d44bb76a7c7166e8deab58a6b4965c420e8ed99c
@justyy <dr.zhihua.lai@gmail.com>
2019-04-04 22:07:13 +0100

    Create 577.sql

---
+++ leetcode/577. Employee Bonus/577.sql
@@ -0,0 +1,11 @@
+# https://helloacm.com/sql-left-outer-join-tutorial-with-example-employee-bonus/
+# https://leetcode.com/problems/employee-bonus/
+
+SELECT
+    Employee.name, Bonus.bonus
+FROM
+    Employee
+        LEFT JOIN
+    Bonus ON Employee.empid = Bonus.empid
+WHERE
+    bonus < 1000
