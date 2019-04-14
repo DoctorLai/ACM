75c6031cf484b05e98de009f51754e8ff3c403ce
@justyy <dr.zhihua.lai@gmail.com>
2019-04-14 19:37:47 +0100

    Create 844.java

---
+++ leetcode/844. Backspace String Compare/844.java
@@ -0,0 +1,19 @@
+// https://helloacm.com/the-backspace-string-compare-algorithm/
+// https://leetcode.com/problems/backspace-string-compare/
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+ 
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
