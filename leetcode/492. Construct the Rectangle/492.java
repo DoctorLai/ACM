3fd396353017b946f2ee9e992e5f48ad181f7ed0
@justyy <dr.zhihua.lai@gmail.com>
2019-04-18 23:12:33 +0100

    Create 492.java

---
+++ leetcode/492. Construct the Rectangle/492.java
@@ -0,0 +1,13 @@
+// https://helloacm.com/the-algorithm-to-construct-the-rectangle-given-its-area/
+// https://leetcode.com/problems/construct-the-rectangle/
+
+class Solution {
+    public int[] constructRectangle(int area) {
+        if (area == 0) return new int[] {0, 0};
+        int x = (int)Math.ceil(Math.sqrt(area));
+        while (area % x != 0) {
+            x ++;
+        }
+        return new int[] { x, area / x };
+    }
+}
