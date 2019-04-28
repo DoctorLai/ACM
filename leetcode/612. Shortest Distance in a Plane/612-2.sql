acda8a871e50827177f9f7161eb14a1d3aefb273
@justyy <dr.zhihua.lai@gmail.com>
2019-04-28 17:01:24 +0100

    Create 612-2.sql

---
+++ leetcode/612. Shortest Distance in a Plane/612-2.sql
@@ -0,0 +1,9 @@
+// https://helloacm.com/sql-algorithm-to-compute-shortest-distance-in-a-plane/
+// https://leetcode.com/problems/shortest-distance-in-a-plane/
+
+select
+  round(sqrt(min(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)))), 2) as shortest
+from
+  point_2d p1
+join
+  point_2d p2 on p1.x != p2.x or p1.y != p2.y;
