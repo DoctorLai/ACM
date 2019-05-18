8e9b5b3f65cb47f18273f91ed1673ee84da3cfee
@justyy <dr.zhihua.lai@gmail.com>
2019-05-18 15:22:30 +0100

    Create 976.cpp

---
+++ leetcode/976. Largest Perimeter Triangle/976.cpp
@@ -0,0 +1,15 @@
+// https://helloacm.com/greedy-algorithm-to-find-the-largest-perimeter-triangle-by-sorting/
+// https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& A) {
+        sort(begin(A), end(A));
+        for (int i = A.size() - 3; i >= 0; -- i) {
+            if (A[i] + A[i + 1] > A[i + 2]) {
+                return A[i] + A[i + 1] + A[i + 2];
+            }
+        }
+        return 0;
+    }
+};
