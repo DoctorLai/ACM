1b81917171c255c0f85cffae7b771574c41e6359
@justyy <dr.zhihua.lai@gmail.com>
2019-07-18 00:50:32 +0100

    Create 170-3.cpp

---
+++ leetcode/170. Two Sum III - Data structure design/170-3.cpp
@@ -0,0 +1,42 @@
+// https://helloacm.com/how-to-design-a-two-sum-data-structure/
+// https://leetcode.com/problems/two-sum-iii-data-structure-design/
+
+class TwoSum {
+public:
+    /** Initialize your data structure here. */
+    TwoSum() {
+        
+    }
+    
+    /** Add the number to an internal data structure.. */
+    void add(int number) {
+       auto it = upper_bound(begin(data), end(data), number);
+       data.insert(it, number);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    bool find(int value) {
+        int lo = 0, hi = data.size() - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (data[lo] + data[hi] == value) {
+                return true;
+            }
+            if (data[lo] + data[hi] > value) {
+                hi --;
+            } else {
+                lo ++;
+            }
+        }
+        return false;
+    }
+private:
+    vector<int> data;
+};
+ 
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
