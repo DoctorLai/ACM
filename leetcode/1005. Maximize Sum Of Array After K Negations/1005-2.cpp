be3dd3249d101555f94a1523427a0628e4e89472
@justyy <dr.zhihua.lai@gmail.com>
2019-07-30 23:11:30 +0100

    Create 1005-2.cpp

---
+++ leetcode/1005. Maximize Sum Of Array After K Negations/1005-2.cpp
@@ -0,0 +1,14 @@
+// https://helloacm.com/maximize-sum-of-array-after-k-negations-using-greedy-algorithm-via-priority-queue-min-element/
+// https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& A, int K) {
+        for (int i = 0; i < K; ++ i) {
+            int m = min_element(begin(A), end(A)) - begin(A);
+            A[m] *= -1;
+        }
+        return std::accumulate(begin(A), end(A), 0, 
+                [](auto &a, auto &b) { return a + b; });
+    }
+};
