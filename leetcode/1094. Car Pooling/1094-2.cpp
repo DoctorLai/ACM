ba32e0178b3b9452af346c68ea1e1c4765b11e68
@justyy <dr.zhihua.lai@gmail.com>
2019-08-01 01:08:56 +0100

    Create 1094-2.cpp

---
+++ leetcode/1094. Car Pooling/1094-2.cpp
@@ -0,0 +1,21 @@
+// https://helloacm.com/passengers-pick-up-and-drop-off-algorithms-car-pooling-via-greedy-algorithm/
+// https://leetcode.com/problems/car-pooling/
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int travel[1001] = { 0 };
+        for (const auto n: trips) {
+            travel[n[1]] += n[0];
+            travel[n[2]] -= n[0];
+        }
+        int cur = 0;
+        for (int i = 0; i < 1001; ++ i) {
+            cur += travel[i];
+            if (cur > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
