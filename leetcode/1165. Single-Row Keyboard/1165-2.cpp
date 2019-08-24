58ea53081c53223e78f749c10fea47e0fbc68f5c
@justyy <dr.zhihua.lai@gmail.com>
2019-08-24 20:46:21 +0100

    Create 1165-2.cpp

---
+++ leetcode/1165. Single-Row Keyboard/1165-2.cpp
@@ -0,0 +1,17 @@
+// https://helloacm.com/single-row-keyboard-algorithms-to-estimate-the-finger-moving-time/
+// https://leetcode.com/problems/single-row-keyboard/submissions/
+
+class Solution {
+public:
+    int calculateTime(string keyboard, string word) {
+        unordered_map<char, int> data;
+        for (int i = 0; i < keyboard.size(); ++ i) {
+            data[keyboard[i]] = i;
+        }
+        int r = data[word[0]];
+        for (int i = 1; i < word.size(); ++ i) {
+            r += abs(data[word[i]] - data[word[i - 1]]);
+        }
+        return r;
+    }
+};
