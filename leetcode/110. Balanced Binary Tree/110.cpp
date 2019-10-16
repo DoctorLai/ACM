bd6bf34f7ebca10eaae6c0fa65c184816ebc5553
@justyy <dr.zhihua.lai@gmail.com>
2019-10-16 15:19:04 +0100

    Create 110.cpp

---
+++ leetcode/110. Balanced Binary Tree/110.cpp
@@ -0,0 +1,28 @@
+// https://helloacm.com/how-to-check-if-a-binary-tree-is-balanced-top-down-and-bottom-up-recursion/
+// https://leetcode.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return depth(root) != -1;
+    }
+private:
+    int depth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int left = depth(root->left);
+        if (left == -1) return -1;
+        int right = depth(root->right);
+        if (right == -1) return -1;
+        if (abs(left - right) > 1) return -1;
+        return max(right, left) + 1;
+    }
+};
