0ec5ec01d4372ecd111bc6ffaf498a82d26ad4bc
@justyy <dr.zhihua.lai@gmail.com>
2020-03-22 15:17:23 +0000

    Create 1387.cpp

---
+++ leetcode/1387. Sort Integers by The Power Value/1387.cpp
@@ -0,0 +1,27 @@
+// https://helloacm.com/dynamic-programming-memoization-to-sort-integers-by-the-power-value/
+// https://leetcode.com/problems/sort-integers-by-the-power-value/submissions/
+// MEDIUM, RECURSION, DYNAMIC PROGRAMMING
+
+class Solution {
+public:
+    int getKth(int lo, int hi, int k) {
+        vector<pair<int, int>> r;
+        for (int i = lo; i &lt;= hi; ++ i) {
+            r.push_back({getSteps(i), i});
+        }
+        sort(begin(r), end(r));
+        return r[k - 1].second;               
+    }
+private:
+    unordered_map<int, int> memo;
+    
+    int getSteps(int n) {
+        if (n == 1) return 0;
+        if (memo.find(n) != memo.end()) {
+            return memo[n];
+        }
+        if (n & 1) {
+            return 1 + getSteps(3 * n + 1);
+        }
+        return 1 + getSteps(n / 2);
+    }
