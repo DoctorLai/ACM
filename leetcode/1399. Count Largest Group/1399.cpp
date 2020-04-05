d19e30328bdfb2ca06671984a1a0cdfd3540347d
@justyy <dr.zhihua.lai@gmail.com>
2020-04-05 15:45:57 +0100

    Create 1399.cpp

---
+++ leetcode/1399. Count Largest Group/1399.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        int count[37] = {};
+        int curMax = 0;
+        int ans = 0;
+        for (int i = 1; i <= n; ++ i) {
+            int j = i;
+            int r = 0;
+            while (j > 0) {
+                r += (j % 10);
+                j /= 10;
+            }
+            count[r] ++;
+            if (count[r] > curMax) {
+                curMax = count[r];
+                ans = 1;
+            } else if (count[r] == curMax) {
+                ans ++;
+            }
+        }
+        return ans;
+    }
+};
