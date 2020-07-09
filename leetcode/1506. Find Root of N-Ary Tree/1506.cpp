29c11045b40e2fed827f63475554e0850e135cae
Doctor Lai <dr.zhihua.lai@gmail.com>
2020-07-09 22:10:17 +0100

    Create 1506.cpp

---
+++ leetcode/1506. Find Root of N-Ary Tree/1506.cpp
@@ -0,0 +1,39 @@
+// https://helloacm.com/how-to-find-root-of-n-ary-tree-using-the-hash-set/
+// https://leetcode.com/problems/find-root-of-n-ary-tree/
+// MEDIUM, HASH SET
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+ 
+    Node() {}
+ 
+    Node(int _val) {
+        val = _val;
+    }
+ 
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+ 
+class Solution {
+public:
+    Node* findRoot(vector<Node*> tree) {
+        unordered_set<Node*> nodes;
+        for (const auto &n: tree) {
+            nodes.insert(n);
+        }
+        for (const auto &n: tree) {
+            for (const auto &c: n->children) {
+                nodes.erase(c);
+            }
+        }
+        return *begin(nodes);
+    }
+};
