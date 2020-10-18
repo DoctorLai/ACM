fade2b793c83d56d35d559632b279cf42b7a7d8c
DoctorLai <dr.zhihua.lai@gmail.com>
2020-10-18 19:01:21 +0100

    Create 1624.cpp

---
+++ leetcode/1624. Largest Substring Between Two Equal Characters/1624.cpp
@@ -0,0 +1,19 @@
+// https://helloacm.com/compute-the-largest-substring-between-two-equal-characters-using-hash-table/
+// https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+// EASY, HASH TABLE
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char, int> data;
+        int ans = -1;
+        for (int i = 0; i < s.size(); ++ i) {
+            if (data.count(s[i])) {
+                ans = max(ans, i - data[s[i]] - 1);
+            } else {
+                data[s[i]] = i;
+            }
+        }
+        return ans;
+    }
+};
