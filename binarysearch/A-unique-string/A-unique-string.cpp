7bf11cac180f285b44385a4c1c55ae6dbe0f9678
DoctorLai <dr.zhihua.lai@gmail.com>
2020-10-25 14:25:24 +0000

    Create A-unique-string.cpp

---
+++ binarysearch/A-unique-string/A-unique-string.cpp
@@ -0,0 +1,12 @@
+// https://helloacm.com/how-to-check-if-a-given-string-has-unique-characters/
+// https://binarysearch.com/problems/A-unique-string
+// EASY, HASH TABLE
+
+bool solve(string s) {
+    unordered_set<char> seen;
+    for (const auto &n: s) {
+        if (seen.count(n)) return false;
+        seen.insert(n);
+    }
+    return true;
+}
