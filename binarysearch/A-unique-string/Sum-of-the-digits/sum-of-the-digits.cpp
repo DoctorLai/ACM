5be7ed138c0faafbd5d8bceceb2537eebf49f734
DoctorLai <dr.zhihua.lai@gmail.com>
2020-10-25 14:52:05 +0000

    Delete sum-of-the-digits.cpp

--- binarysearch/A-unique-string/Sum-of-the-digits/sum-of-the-digits.cpp
+++
@@ -1,7 +0,0 @@
-// https://helloacm.com/algorithms-to-compute-the-sum-of-the-digits/
-// https://binarysearch.com/problems/Sum-of-the-digits
-// EASY, MATH, RECURSIVE
-
-int solve(int num) {
-    return num == 0 ? 0 : (num % 10) + solve(num / 10);
-}
