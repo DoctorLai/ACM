23ea7baa61508ff51418ebe6024dedf28e224e09
DoctorLai <dr.zhihua.lai@gmail.com>
2020-10-26 19:48:07 +0000

    Create Trie.cpp

---
+++ binarysearch/Trie/Trie.cpp
@@ -0,0 +1,46 @@
+// https://helloacm.com/c-implementation-of-trie-data-structure-using-hash-map/
+// https://binarysearch.com/problems/Trie
+// MEDIUM, TRIE
+
+class Trie {
+    public:
+    Trie() {
+        leaf = false;
+    }
+
+    unordered_map<char, Trie*> children;
+    bool leaf;
+    
+    void add(string s) {
+        auto root = this;
+        for (const auto &n: s) {
+            if (!root->children[n]) {
+                root->children[n] = new Trie();
+            }
+            root = root->children[n];
+        }
+        root->leaf = true;
+    }
+
+    bool exists(string word) {
+        auto root = this;
+        for (const auto &n: word) {
+            if (!root->children[n]) {
+                return false;
+            }
+            root = root->children[n];
+        }        
+        return root && root->leaf;
+    }
+
+    bool startswith(string p) {
+        auto root = this;
+        for (const auto &n: p) {
+            if (!root->children[n]) {
+                return false;
+            }
+            root = root->children[n];
+        }        
+        return root != NULL;      
+    }
+};
