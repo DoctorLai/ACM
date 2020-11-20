f1542600183db25a72f535596a117a14d8e94ea2
DoctorLai <dr.zhihua.lai@gmail.com>
2020-11-20 20:07:09 +0000

    Create Make-Strings-Same.cpp

---
+++ binarysearch/Make-Strings-Same/Make-Strings-Same.cpp
@@ -0,0 +1,17 @@
+// https://helloacm.com/can-we-make-strings-same-by-unlimited-number-of-swaps/
+// https://binarysearch.com/problems/Make-Strings-Same
+// EASY, STRING, HASH MAP
+
+bool solve(string s, string t) {
+    unordered_map<char, int> data;
+    for (const auto &n: s) {
+        data[n] ++;
+    }
+    for (const auto &n: t) {
+        data[n] ++;
+    }
+    for (const auto &[a, b]: data) {
+        if (b & 1) return false;
+    }
+    return true;
+}
