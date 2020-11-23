774d9de846fb62941ea4fbca42be36806451aae8
DoctorLai <dr.zhihua.lai@gmail.com>
2020-11-23 20:46:36 +0000

    Create 159.cpp

---
+++ leetcode/159. Longest Substring with At Most Two Distinct Characters/159.cpp
@@ -0,0 +1,24 @@
+// https://helloacm.com/two-pointer-sliding-window-to-compute-the-longest-substring-with-at-most-two-distinct-characters/
+// https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
+// MEDIUM, SLIDING WINDOW, TWO POINTER, HASH TABLE
+
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        unordered_map<char, int> window;
+        int left = 0, right = 0;
+        int ans = 0;
+        while (right < s.size()) {
+            window[s[right]] ++;       
+            while ((left <= right) && (window.size() &t; 2)) {
+                if (-- window[s[left]] == 0) {
+                    window.erase(s[left]);
+                }
+                left ++;
+            }                      
+            ans = max(ans, right - left + 1);
+            right ++;
+        }
+        return ans;
+    }
+};
