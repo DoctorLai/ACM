f134f539e519b8a8000f80ce52ae303344788256
DoctorLai <dr.zhihua.lai@gmail.com>
2020-12-18 10:36:01 +0000

    Create Word-Formation.cpp

---
+++ binarysearch/Word-Formation/Word-Formation.cpp
@@ -0,0 +1,24 @@
+// https://helloacm.com/word-formation-algorithm-using-hash-map/
+// https://binarysearch.com/problems/Word-Formation
+// EASY, HASH MAP
+
+int solve(vector<string>& words, string letters) {
+    unordered_map<char, int> count;        
+    for (auto &n: letters) {
+        count[n] ++;
+    }
+    function<bool(string &)> check = [&](string &a) {
+        auto data = count;
+        for (auto &n: a) {
+            if (data[n] -- == 0) return false;
+        }
+        return true;
+    };
+    int ans = 0;
+    for (auto &n: words) {
+        if (check(n)) {
+            ans = max(ans, (int)n.size());
+        }
+    }
+    return ans;
+}
