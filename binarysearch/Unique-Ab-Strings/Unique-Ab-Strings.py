b3b3bbfdc94b17b4e2b7f04bb6ce019ad78f8aba
DoctorLai <dr.zhihua.lai@gmail.com>
2021-01-16 15:00:28 +0000

    Create Unique-Ab-Strings.py

---
+++ binarysearch/Unique-Ab-Strings/Unique-Ab-Strings.py
@@ -0,0 +1,8 @@
+# https://binarysearch.com/problems/Unique-Ab-Strings
+# https://helloacm.com/count-the-unique-ab-strings-by-permutation/
+# MATH
+
+class Solution:
+    def solve(self, s):
+        a = sum([1 for x in s if x == 'a'])
+        return 2**a
