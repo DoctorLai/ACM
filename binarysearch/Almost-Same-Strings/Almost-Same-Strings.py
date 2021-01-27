3316d5bff5c779634193922ee036fb30f1f294ab
DoctorLai <dr.zhihua.lai@gmail.com>
2021-01-27 18:00:27 +0000

    Create Almost-Same-Strings.py

---
+++ binarysearch/Almost-Same-Strings/Almost-Same-Strings.py
@@ -0,0 +1,14 @@
+# https://helloacm.com/hash-algorithm-to-check-a-list-containing-almost-same-strings-differ-by-one-character/
+# https://binarysearch.com/problems/Almost-Same-Strings
+# MEDIUM, HASH MAP
+
+class Solution:
+    def solve(self, words):
+        seen = set()
+        for i in words:
+            for j in range(len(i)):
+                s = i[:j] + "*" + i[j + 1:]
+                if s in seen:
+                    return True
+                seen.add(s)
+        return False
