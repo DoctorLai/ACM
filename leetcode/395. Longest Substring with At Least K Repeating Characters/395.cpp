69f7ae73431d25cb28aeb646cc335b4df70e4576
DoctorLai <dr.zhihua.lai@gmail.com>
2021-02-26 21:34:09 +0000

    Create 395.cpp

---
+++ leetcode/395. Longest Substring with At Least K Repeating Characters/395.cpp
@@ -0,0 +1,21 @@
+// https://helloacm.com/compute-longest-substring-with-at-least-k-repeating-characters-via-divide-and-conquer-algorithm/
+// https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
+// MEDIUM, DIVIDE AND CONQUER
+
+class Solution {
+    public:
+    int longestSubstring(string s, int k) {
+        unordered_map<char, int> freq;
+        for (auto &n: s) {
+            freq[n] ++;
+        }
+        for (auto &[a, b]: freq) {
+            if (b < k) {
+                int i = 0;
+                while (i < s.size() && (s[i] != a)) i ++;
+                return max(longestSubstring(s.substr(0, i), k), longestSubstring(s.substr(i + 1), k));
+            }
+        }
+        return s.size();
+    }
+};
