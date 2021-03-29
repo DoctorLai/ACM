69dc604d7cef096dcf365ce3f167de492ad53a22
DoctorLai <dr.zhihua.lai@gmail.com>
2021-03-29 13:59:19 +0100

    Create 1796.cpp

---
+++ leetcode/1796. Second Largest Digit in a String/1796.cpp
@@ -0,0 +1,17 @@
+// https://helloacm.com/finding-the-second-largest-digit-in-a-string/
+// https://leetcode.com/problems/second-largest-digit-in-a-string
+// EASY, STRING, HASH TABLE
+
+class Solution {
+public:
+    int secondHighest(string s) {
+        set<int> data;
+        for (auto &n: s) {
+            if (isdigit(n)) {
+                data.insert(n - 48);
+            }
+        }
+        if (data.size() <= 1) return -1;
+        return *prev(prev(end(data)));
+    }
+};
