1591c5fe86ee4e3a6c3bb818d121dcade75d8c94
DoctorLai <dr.zhihua.lai@gmail.com>
2021-05-04 21:07:34 +0100

    Create 872.py

---
+++ leetcode/872. Leaf-Similar Trees/872.py
@@ -0,0 +1,20 @@
+# https://helloacm.com/teaching-kids-programming-leaf-similar-trees-by-recursive-depth-first-search-algorithm/
+# https://leetcode.com/problems/leaf-similar-trees/
+# EASY, DFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+        def dfs(node):
+            if not node:
+                return
+            if not node.left and not node.right:
+                yield node.val
+            yield from dfs(node.left)
+            yield from dfs(node.right)
+        return list(dfs(root1)) == list(dfs(root2))
