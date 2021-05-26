ae493011875ae3b444ad8126cf3fe9bb0c6113f8
DoctorLai <dr.zhihua.lai@gmail.com>
2021-05-26 21:34:26 +0100

    Create 1832.py

---
+++ leetcode/1832. Check if the Sentence Is Pangram/1832.py
@@ -0,0 +1,11 @@
+# https://helloacm.com/teaching-kids-programming-check-if-the-sentence-is-pangram/
+# https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+# EASY, STRING, HASH TABLE
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        c = Counter(sentence)
+        for i in range(26):
+            if c[chr(i + 97)] == 0:
+                return False
+        return True
