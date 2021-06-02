00bea4e5744a4270f1511998f5f89730ace9eeb2
DoctorLai <dr.zhihua.lai@gmail.com>
2021-06-02 19:40:09 +0100

    Create N-Rooks.py

---
+++ binarysearch/N-Rooks/N-Rooks.py
@@ -0,0 +1,7 @@
+# https://helloacm.com/teaching-kids-programming-permutation-of-rooks-do-not-attack-each-other/
+# https://binarysearch.com/problems/N-Rooks
+# EASY, MATH
+
+class Solution:
+    def solve(self, n):
+        return math.factorial(n)
