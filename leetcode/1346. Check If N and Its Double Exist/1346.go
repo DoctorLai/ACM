8ff68ee5cdb8b7b54a03ec823526e8de0b361eb1
DoctorLai <dr.zhihua.lai@gmail.com>
2021-06-08 12:39:08 +0100

    Create 1346.go

---
+++ leetcode/1346. Check If N and Its Double Exist/1346.go
@@ -0,0 +1,20 @@
+// https://helloacm.com/golang-check-if-n-and-its-double-exist-hash-map/
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
+// EASY, HASH TABLE
+
+func checkIfExist(arr []int) bool {
+    var data = make(map[int]int)
+    for _, n := range arr {
+        data[n] ++
+    }
+    for key, value := range data {
+        if key == 0 {
+            if value > 1 {
+                return true
+            }
+        } else if _, ok := data[key * 2]; ok {
+            return true
+        }
+    }
+    return false
+}
