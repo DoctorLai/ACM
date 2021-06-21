c8204f2901ebef368735d32040ec7eb19d2d1e32
DoctorLai <dr.zhihua.lai@gmail.com>
2021-06-21 20:33:51 +0100

    Create Swap-Characters-to-Equalize-Strings.py

---
+++ binarysearch/Swap-Characters-to-Equalize-Strings/Swap-Characters-to-Equalize-Strings.py
@@ -0,0 +1,9 @@
+# https://helloacm.com/teaching-kids-programming-swap-characters-to-equalize-strings/
+# https://binarysearch.com/problems/Swap-Characters-to-Equalize-Strings
+# EASY, STRING, HASH MAP
+
+class Solution:
+    def solve(self, s, t):
+        a = Counter(s)
+        a.update(t)
+        return all(i & 1 == 0 for i in a.values())
