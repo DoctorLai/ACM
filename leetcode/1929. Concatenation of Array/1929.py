96ebc381e868dfc38023fe2494ca3229780f7dbf
DoctorLai <dr.zhihua.lai@gmail.com>
2021-07-21 20:28:30 +0100

    Create 1929.py

---
+++ leetcode/1929. Concatenation of Array/1929.py
@@ -0,0 +1,7 @@
+# https://helloacm.com/teaching-kids-programming-concatenation-of-arrays/
+# https://leetcode.com/problems/concatenation-of-array/
+# EASY, ARRAY
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums * 2
