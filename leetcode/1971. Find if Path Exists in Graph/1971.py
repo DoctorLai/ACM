4cd9a73babce68ab9608726d139c08fd0f73faa4
DoctorLai <dr.zhihua.lai@gmail.com>
2021-08-18 21:17:10 +0100

    Create 1971.py

---
+++ leetcode/1971. Find if Path Exists in Graph/1971.py
@@ -0,0 +1,22 @@
+# https://helloacm.com/teaching-kids-programming-find-if-path-exists-in-graph-via-breadth-first-search-algorithm/
+# https://leetcode.com/problems/find-if-path-exists-in-graph/
+# EASY, BFS, Graph
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        G = defaultdict(list[int])
+        for s,e in edges:
+            G[s].append(e)
+            G[e].append(s)        
+        seen = set()
+        q = deque([start])
+        while q:
+            c = q.popleft()
+            if c == end:
+                return True
+            if c in seen:
+                continue
+            seen.add(c)
+            for i in G[c]:
+                q.append(i)
+        return False
