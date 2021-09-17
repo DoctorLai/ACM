7d3ef0a4dccf6a373956e7ae1fbc807ecbb6211e
DoctorLai <dr.zhihua.lai@gmail.com>
2021-09-17 20:54:09 +0100

    Update Swap-Characters-to-Equalize-Strings.py

--- binarysearch/Swap-Characters-to-Equalize-Strings/Swap-Characters-to-Equalize-Strings.py
+++ binarysearch/Swap-Characters-to-Equalize-Strings/Swap-Characters-to-Equalize-Strings.py
@@ -1,6 +1,6 @@
 # https://helloacm.com/teaching-kids-programming-swap-characters-to-equalize-strings/
 # https://binarysearch.com/problems/Swap-Characters-to-Equalize-Strings
-# EASY, STRING, HASH MAP
+# EASY, STRING, HASH TABLE
 
 class Solution:
     def solve(self, s, t):
