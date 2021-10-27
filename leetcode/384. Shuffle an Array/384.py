24321e6883314f6319c9f232654f356c3b882204
DoctorLai <dr.zhihua.lai@gmail.com>
2021-10-27 21:56:11 +0100

    Create 384.py

---
+++ leetcode/384. Shuffle an Array/384.py
@@ -0,0 +1,19 @@
+# https://helloacm.com/teaching-kids-programming-the-fisher-yates-random-shuffle-algorithm-in-python/
+# https://leetcode.com/problems/shuffle-an-array/
+# MEDIUM, RANDOM
+
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        for i in range(len(self.array)):
+            swap_idx = random.randrange(i, len(self.array))
+            self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
+        return self.array
