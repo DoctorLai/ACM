fff7cb375704d4d600bae5c6e931ebbb62b1c3f6
justyy <dr.zhihua.lai@gmail.com>
2022-05-25 11:26:50 +0000

    Index Regenerated Wed 25 May 2022 11:26:50 AM UTC

--- binarysearch/Index Into an Infinite String/Index Into an Infinite String.py
+++
@@ -1,7 +0,0 @@
-# https://helloacm.com/teaching-kids-programming-index-into-an-infinite-string-find-substring/
-# https://binarysearch.com/problems/Index-Into-an-Infinite-String
-# EASY, MATH
-
-class Solution:
-    def solve(self, s, i, j):
-        return "".join(s[x % len(s)] for x in range(i, j))
