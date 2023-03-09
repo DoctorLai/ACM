a06914ee41687793b76c4c5531cf6f454e95b22a
DoctorLai <dr.zhihua.lai@gmail.com>
2023-03-09 18:15:03 +0000

    Update 627.sql

--- leetcode/627. Swap Salary/627.sql
+++ leetcode/627. Swap Salary/627.sql
@@ -1,4 +1,5 @@
 # https://helloacm.com/the-mysql-if-and-case-statement-exercise-how-to-swap-elements-in-mysql/
 # https://leetcode.com/problems/swap-salary/
+# EASY, SQL/
 
 UPDATE salary SET sex = IF(sex='m','f','m');
