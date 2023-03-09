7d30c2996efa7e3ccf2bb2b293e28f518f233c7d
DoctorLai <dr.zhihua.lai@gmail.com>
2023-03-09 18:16:22 +0000

    Update 608-3.sql

--- leetcode/608. Tree Node/608-3.sql
+++ leetcode/608. Tree Node/608-3.sql
@@ -1,9 +1,9 @@
 # https://helloacm.com/how-to-determine-the-type-of-tree-nodes-using-sql/
 # https://leetcode.com/problems/tree-node/
+# MEDIUM, SQL
 
 select id, (case
     when p_id is null then "Root"
     when exists (select * from tree as t2 where t1.id = t2.p_id) then "Inner"
     else "Leaf"
-end) as Type from tree as t1
-
+end) as Type from tree as t1;
