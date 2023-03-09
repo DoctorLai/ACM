ab315d48d37ac9bff3d6a5a795aaab33cbc23a8a
DoctorLai <dr.zhihua.lai@gmail.com>
2023-03-09 18:12:03 +0000

    Update 612-3.sql

--- leetcode/612. Shortest Distance in a Plane/612-3.sql
+++ leetcode/612. Shortest Distance in a Plane/612-3.sql
@@ -1,5 +1,6 @@
-// https://helloacm.com/sql-algorithm-to-compute-shortest-distance-in-a-plane/
-// https://leetcode.com/problems/shortest-distance-in-a-plane/
+# https://helloacm.com/sql-algorithm-to-compute-shortest-distance-in-a-plane/
+# https://leetcode.com/problems/shortest-distance-in-a-plane/
+# MEDIUM, SQL
 
 select 
   round(min(sqrt(pow(a.x - b.x,2) + pow(a.y - b.y,2))), 2) as 'shortest'
@@ -7,4 +8,4 @@ from
   point_2d a, point_2d b
 where
   (a.x != b.x or a.y != b.y) and
-  (a.x >= b.x)
+  (a.x >= b.x);
