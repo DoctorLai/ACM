4b3ec3c9e9605a9420b08e647268c0d503a00625
DoctorLai <dr.zhihua.lai@gmail.com>
2023-03-09 18:10:50 +0000

    Update 577.sql

--- leetcode/577. Employee Bonus/577.sql
+++ leetcode/577. Employee Bonus/577.sql
@@ -1,5 +1,6 @@
 # https://helloacm.com/sql-left-outer-join-tutorial-with-example-employee-bonus/
 # https://leetcode.com/problems/employee-bonus/
+# EASY, SQL
 
 SELECT
     Employee.name, Bonus.bonus
@@ -8,4 +9,4 @@ FROM
         LEFT JOIN
     Bonus ON Employee.empid = Bonus.empid
 WHERE
-    bonus < 1000
+    bonus < 1000;
