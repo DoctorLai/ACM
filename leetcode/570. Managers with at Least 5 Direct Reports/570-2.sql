3f21daf7112047fd1d512c695b9ecda833375c39
DoctorLai <dr.zhihua.lai@gmail.com>
2023-03-09 18:13:45 +0000

    Update 570-2.sql

--- leetcode/570. Managers with at Least 5 Direct Reports/570-2.sql
+++ leetcode/570. Managers with at Least 5 Direct Reports/570-2.sql
@@ -1,9 +1,11 @@
-// https://helloacm.com/sql-inner-join-tutorial-with-example-managers-with-at-least-5-direct-reports/
-// https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
+# https://helloacm.com/sql-inner-join-tutorial-with-example-managers-with-at-least-5-direct-reports/
+# https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
+# MEDIUM, SQL
 
-select Name from
-Employee where Id in (
- select ManagerId from Employee
- group by ManagerId
- having count(ManagerId) >= 5
-)
+select Name 
+from Employee 
+where Id in (
+  select ManagerId from Employee
+  group by ManagerId
+  having count(ManagerId) >= 5
+);
