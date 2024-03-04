3673fa51ddb9fc05f1eaf15731ea1f7f65d81edf
DoctorLai <dr.zhihua.lai@gmail.com>
2024-03-04 14:11:28 +0000

    Update DSU.cpp

--- template/DSU.cpp
+++ template/DSU.cpp
@@ -41,3 +41,46 @@ private:
     vector<int> rank;
     int count;
 };
+
+class UnionFind {
+    public:
+        UnionFind(int n): N(n) {
+            data.resize(n);
+            sizes.resize(n, 0);
+            std::iota(begin(data), end(data), 0);
+        }
+
+        bool merge(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+            if (pa == pb) {
+                return false;
+            }
+            int sa = sizes[pa];
+            int sb = sizes[pb];
+            if (sa >= sb) {
+                swap(pa, pb);
+            }
+            data[pa] = pb;
+            sizes[pb] ++;
+            return true;
+        }
+
+        int find(int x) {
+            if (x != data[x]) {
+                data[x] = find(data[x]);
+            }
+            return data[x];
+        }
+
+        int size(int x) {
+            return count_if(begin(data), end(data), [&](auto a) { 
+                return data[x] == a; 
+            });
+        }
+
+    private:
+        vector<int> data;
+        vector<int> sizes;
+        int N;    
+};
