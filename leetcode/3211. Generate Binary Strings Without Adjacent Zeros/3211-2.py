d027c9bb26ea4f9a7dd907afd41587c697b3a510
DoctorLai <dr.zhihua.lai@gmail.com>
2025-02-28 21:01:12 +0000

    Create 3211-2.py

---
+++ leetcode/3211. Generate Binary Strings Without Adjacent Zeros/3211-2.py
@@ -0,0 +1,22 @@
+# https://helloacm.com/teaching-kids-programming-another-bfs-to-generate-binary-strings-without-adjacent-zeros/
+# https://leetcode.com/problems/generate-binary-strings-without-adjacent-zeros/
+# MEDIUM, BFS, RECURSION
+
+from typing import List
+ 
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        if n == 0:
+            return []
+        if n == 1:
+            return ["0", "1"]
+        
+        ans = ["0", "1"]
+        for i in range(1, n):
+            cur = []
+            for x in ans:
+                if x[-1] == '1':
+                    cur.append(x + "0")
+                cur.append(x + "1")
+            ans = cur
+        return ans
